4520018252601c682808098b9974bfb409d47d14
rmadeye <rafal.madaj@protonmail.com>
2023-10-26 23:22:45 +0200

    upd

--- run.py
+++ run.py
@@ -17,30 +17,19 @@ if __name__ == "__main__":
         selected_var = int(input("Enter index of column to be used as independent feature: "))
         values = ""
         while True:
-            values = input("Enter values for prediction (sep by comma): ")
+            values = input("Enter values for prediction (sep by comma) or KONIEC to exit: ")
             if values == "KONIEC":
                 break
             values = [int(x) for x in values.split(",")]
             y_pred = ac.select_model(model_choice, selected_var, values)
-            # breakpoint()
             print(f"Predicted value: {y_pred}")
 
-        # data = load_iris()["data"]
-        # db = DBCreator("iris", data.shape[1], "a,b,c,d")
-
-        # print("Select column to be used as independent feature (y)")
-        # col_vals = db.show_data().columns.values
-        # print(col_vals)
-        # y_choice = int(input(f"{[i for i in range(len(col_vals))]}: "))
-        # columns_to_keep = [i for i in range(data.shape[1]) if i != y_choice]
-        # y = data[:, y_choice]
-        # X = data[:, columns_to_keep]
-
-        # model_choice = input("Select model: lr or lr: ")
-        # if model_choice == "lr":
-        #     lr = LinReg(X, y).prepare_model()
-        #     vals = input(
-        #         f"Enter values for prediction (column names:) {db.show_data().iloc[:,[x for x in range(0,db.show_data().shape[1]) if x != y_choice]].columns.values},  separated by comma: "
-        #     )
-        #     vals = np.array([int(x) for x in vals.split(",")]).reshape(1, -1)
-        #     print(f"Predicted value: {lr[0].predict(vals)[0]}")
+    elif ans1.upper() == 'N':
+        print("Enter database name: ")
+        db_name = input()
+        print("Enter number of features: ")
+        feature_number = input()
+        print("Enter feature names separated by comma: ")
+        feature_names = input()
+        db = DBCreator(db_name, feature_number, feature_names)
+        
--- src/ui.py
+++ src/ui.py
@@ -33,3 +33,52 @@ class AutomaticChosen:
             vals = lr[1].transform(vals)
             output = lr[0].predict(vals)
             return output[0]
+        
+
+class ManualChosen:
+    db_name: str = ""
+    feature_number: int
+    feature_names: list
+
+    def __init__(self) -> None:
+        self.db_name = input("Enter database name: ")
+        self.feature_number = int(input("Enter number of features: "))
+        self.feature_names = input("Enter feature names separated by comma: ")
+        self.feature_names = self.feature_names.split(",")
+        assert self.db_name, "Database must have a name"
+        assert self.feature_number >= 2, "Feature number too small"
+        assert len(self.feature_names) == self.feature_number
+
+        self.df = pd.DataFrame(columns=self.feature_names)
+
+    def _add_entry(self, features: list):
+        self.df = pd.concat(
+            [self.df, pd.DataFrame([features], columns=self.feature_names)],
+            ignore_index=True,
+        )
+
+    def get_data_from_input(self):
+        features = input("Enter your features values separated by comma")
+        features = features.split(",")
+        self._add_entry(features)
+
+
+    def split_dataset(self, selected_var: int):
+        columns_to_keep = [i for i in range(self.df.shape[1]) if i != selected_var]
+
+        return self.df[:, columns_to_keep], self.df[:, selected_var]
+    
+    def select_model(
+        self,
+        chosen_model: str = "lm",
+        selected_var: int = -1,
+        values_to_predict: list = [0, 0, 0],
+    ):
+        X, y = self.split_dataset(selected_var)
+        if chosen_model == "lm":
+            lr = LinReg(X, y).prepare_model()
+            vals = np.array(values_to_predict).reshape(-1, 1).reshape(1, -1)
+            assert type(vals) == np.ndarray
+            vals = lr[1].transform(vals)
+            output = lr[0].predict(vals)
+            return output[0]
